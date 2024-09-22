fb6599f2150eaae53c293d041b1829155d1cb1bb
Atef-Abrar-Bhuyian <abrarbhuyian8@gmail.com>
2024-09-23 01:12:24 +0600

    home page link modified

--- js/login.js
+++ js/login.js
@@ -16,7 +16,7 @@ document.getElementById('btn-login').addEventListener('click', function(event){
     if(phoneNumber === '5' && pinNumber === '1234'){
         // console.log('You are logged in');
         // step 5: allow user to use the website
-        window.location.href='/home.html';
+        window.location.href='./home.html';
     }
     else{
         alert('wrong phone number or pin');
