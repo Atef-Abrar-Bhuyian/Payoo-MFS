a6912fa24e169643fd6684c63ee8f5444a466917
Atef-Abrar-Bhuyian <abrarbhuyian8@gmail.com>
2024-09-22 23:21:48 +0600

    log in js done

--- js/login.js
+++ js/login.js
@@ -4,9 +4,21 @@
 document.getElementById('btn-login').addEventListener('click', function(event){
     // Step 2: prevent default behavior (Prevent reloading browser)
     event.preventDefault() // login to not reload
-    console.log('log in btn clicked');
 
     // step 3: get the phone number
     const phoneNumber = document.getElementById('phone-number').value;
-    console.log(phoneNumber);
+    const pinNumber = document.getElementById('pin-number').value;
+    // console.log(phoneNumber);
+    // console.log(pinNumber);
+
+    // step 4: validate phone and pin
+    // this is temporary & we should not do like this
+    if(phoneNumber === '5' && pinNumber === '1234'){
+        // console.log('You are logged in');
+        // step 5: allow user to use the website
+        window.location.href='/home.html';
+    }
+    else{
+        alert('wrong phone number or pin');
+    }
 })
